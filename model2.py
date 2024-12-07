952645c04e7d167301fa6fe2d28480b388c71140
Feinst69 <157037115+Feinst69@users.noreply.github.com>
2024-12-07 21:50:27 +0300

    [update] modify data loading in model2.py to read from CSV and adjust target variable extraction

--- model2.py
+++ model2.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pandas as pd
-from perceptron2 import artificial_neuron, load_data
-from sklearn.model_selection import train_test_split
+from perceptron2 import artificial_neuron
 
-# Load and preprocess dataset
-X, y = load_data('forward_selected_features.csv')
+df = pd.read_csv('forward_selected_features.csv')
+print(df.columns)
+X = df.drop('diagnosis', axis=1).values
+y = df.pop('diagnosis').values
 
-# Split dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train perceptron
-W, b = artificial_neuron(X_train, y_train, learning_rate=0.01, n_iter=1000)
+W, b = artificial_neuron(X, y, learning_rate=0.01, n_iter=1000)
 
 # # Evaluate perceptron
 # y_pred = (model(X_test, W, b) >= 0.5).astype(int)
