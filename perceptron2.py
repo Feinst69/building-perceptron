d629dcf0d92c5efa551a818c96dd185e0832d925
Feinst69 <157037115+Feinst69@users.noreply.github.com>
2024-12-07 20:17:38 +0300

    remove decision boundary plotting from artificial neuron training process

--- perceptron2.py
+++ perceptron2.py
@@ -31,17 +31,6 @@ def predict(X, W, b):
     # print(A)
     return A >= 0.5
 
-def plot_decision_boundary(X, y, W, b, iteration):
-    x0_min, x0_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x1_min, x1_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx0, xx1 = np.meshgrid(np.arange(x0_min, x0_max, 0.01),
-                           np.arange(x1_min, x1_max, 0.01))
-    grid = np.c_[xx0.ravel(), xx1.ravel()]
-    probs = model(grid, W, b).reshape(xx0.shape)
-    plt.contourf(xx0, xx1, probs, alpha=0.8, levels=[0, 0.5, 1], cmap='RdBu', vmin=0, vmax=1)
-    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', marker='o', cmap='RdBu')
-    plt.title(f'Iteration {iteration}')
-    plt.show()
 
 def artificial_neuron(X, y, learning_rate=0.1, n_iter=100):
     # initialisation W, b
@@ -54,9 +43,6 @@ def artificial_neuron(X, y, learning_rate=0.1, n_iter=100):
         Loss.append(log_loss(A, y))
         dW, db = gradients(A, X, y)
         W, b = update(dW, db, W, b, learning_rate)
-        
-        # Plot decision boundary at each iteration
-        plot_decision_boundary(X, y, W, b, i)
 
     y_pred = predict(X, W, b)
     print(accuracy_score(y, y_pred))
